612f5b0ea791e577d82fe4f2e53398b0b7ccbbb0
ErickDsBR <erick.brasil2102@gmail.com>
2026-08-02 01:12:37 -0300

    refactor(app): wire navigation and home page into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Auth } from "./components/auth/Auth";
-
+import { Nav } from "./components/Navgation/navgation.tsx";
+import { Home } from "./pages/home/home.tsx";
 export function App() {
-  return <Auth></Auth>;
+  return (
+    <>
+      <Nav></Nav>
+      <Home></Home>
+    </>
+  );
 }
