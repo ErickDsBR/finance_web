6f9db72e89b01201f2e27ed6b9661447d31c29aa
ErickDsBR <erick.brasil2102@gmail.com>
2026-08-02 01:12:37 -0300

    refactor(auth): remove legacy auth component

--- src/components/auth/Auth.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./auth.module.css";
-
-export function Auth() {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.header}>Auth</h1>
-    </div>
-  );
-}
