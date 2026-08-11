6522e864480317f482642d52cfbbba2dbf1b5a25
ErickDsBR <erick.brasil2102@gmail.com>
2026-08-10 23:50:13 -0300

    feat(home): delay welcome heading animation

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,7 @@
 export function Home() {
   return (
     <div className="home">
-      <h1 className="flex items-center justify-center text-5xl text-blue-600/75 pt-8 motion-preset-slide-up-lg ">
+      <h1 className="flex items-center justify-center text-5xl text-blue-600/75 pt-8 motion-preset-slide-up-lg motion-delay-300">
         Welcome to the Cash In
       </h1>
     </div>
